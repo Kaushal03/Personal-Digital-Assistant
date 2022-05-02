556542b67109da040fb022767a8c919535f699f0
Kaushal Sethia <kaushalsethia01@gmail.com>
2022-05-02 05:23:57 -0700

    Update Personal_Digital_Assistant.py

--- Personal_Digital_Assistant.py
+++ Personal_Digital_Assistant.py
@@ -4,7 +4,12 @@ import gtts
 import playsound
 import pyttsx3
 import pywhatkit
-
+import datetime
+import wikipedia
+import webbrowser
+from bs4 import BeautifulSoup
+import requests
+import os
 
 engine = pyttsx3.init()
 voices = engine.getProperty('voices')
@@ -12,6 +17,83 @@ engine.setProperty('voice', voices[1].id)
 
 recognizer = sr.Recognizer()
 microphone=sr.Microphone()
+
+def image_download():
+    usr_agent = {
+        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+        'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
+        'Accept-Encoding': 'none',
+        'Accept-Language': 'en-US,en;q=0.8',
+        'Connection': 'keep-alive',
+    }
+    
+    microphone=sr.Microphone()
+    recognizer = sr.Recognizer()
+    with sr.Microphone() as source:
+        speak('whose images should I download for you sir')
+        speak('Speak Now')
+        print('speak')
+        recognizer.adjust_for_ambient_noise(source)
+        voice = recognizer.listen(source)
+        print(voice)
+        print('Recognizing your voice')
+        text = recognizer.recognize_google(voice, language='en')
+        print('Wait....')
+        print(text)
+        speak('downloading images for'+text)
+        pywhatkit.playonyt(text)
+    n_images=50
+    img_format = 'png'
+    folder_name = text
+    def scrappi(data, n_images, img_format, folder_name):
+            
+        #URL = ['https://www.google.com/search?site=&tbm=isch&source=hp&biw=1873&bih=990&q=']
+        URL = ['https://www.google.com/search?tbm=isch&q=']
+        
+        def check(folder_name):
+            try: 
+                os.mkdir(folder_name)
+                return folder_name
+            except:
+                print("Folder Exist with that name!")
+                folder_name = input("Enter a new Folder name: \n")
+                try: 
+                    os.mkdir(folder_name)
+                    return folder_name
+                except: return check(folder_name)
+                
+        folder_name = check(folder_name)
+
+        print('Starting to Download...')
+
+        for i in URL:
+            searchurl = i + str(data)
+            response = requests.get(searchurl, headers = usr_agent)
+            html = response.text
+            soup = BeautifulSoup(html, 'html.parser')
+            results = soup.findAll('img', limit = n_images + 1)
+
+            if len(results) != 0:
+                for i, image in enumerate(results):
+                    try: image_link = image["data-srcset"]
+                    except:
+                        try: image_link = image["data-src"]
+                        except:
+                            try: image_link = image["data-fallback-src"]
+                            except:
+                                try: image_link = image["src"]
+                                except: pass
+                        try:
+                            r = requests.get(image_link).content
+                            try: r = str(r, 'utf-8')
+                            except UnicodeDecodeError:
+                                with open(f"{folder_name}/images{i}.{img_format}", "wb+") as f: f.write(r)
+                        except: pass
+                        
+        return 'Successfully Downloaded ' + str(n_images) + ' images'
+    scrappi(text,n_images,img_format,folder_name)
+
 
 def speak(audio):
     engine.say(audio)
@@ -33,7 +115,18 @@ def song():
         speak('playing song'+text)
         pywhatkit.playonyt(text)
 
-    
+def tellDay():
+    day = datetime.datetime.today().weekday() + 1
+
+    Day_dict = {1: 'Monday', 2: 'Tuesday', 
+                3: 'Wednesday', 4: 'Thursday', 
+                5: 'Friday', 6: 'Saturday',
+                7: 'Sunday'}
+        
+    if day in Day_dict.keys():
+        day_of_the_week = Day_dict[day]
+        print(day_of_the_week)
+        speak("The day is " + day_of_the_week) 
 
 def translation():
     microphone=sr.Microphone()
@@ -79,7 +172,8 @@ def start():
             elif (text==str('how are you')):
                 speak("I am fine")
                 start()
-            elif (text==str('What is your Name') or text==str('Your Name') or text==str('name')):
+            #elif (text==str('What is your Name') or text==str('Your Name') or text==str('name')):
+            elif 'your name' in text:
                 speak('My Name is KAKATONA')
                 start()
             elif (text==str('Quit') or text==str('turn off') or text==str('thank you') or text==str('exit')):
@@ -90,10 +184,45 @@ def start():
             elif (text==str('play song')):
                 song()
                 start()
-            else:
+            elif 'time' in text:
+                time = datetime.datetime.now().strftime('%I: %M')
+                print(time)
+                speak('current time is' + time)
+                start()
+            elif 'tell me about' in text or 'about' in text:
+                thing = text.replace('tell me about', '')
+                info = wikipedia.summary(thing, 2)
+                print(info)
+                speak(info)
+                start()
+            elif 'who are you' in text:
+                speak('I am your personal Assistant KAKATONA')
+                start()
+            elif 'what can you do for me' in text:
+                speak('I can play songs, tell time, and help you go with wikipedia')
+                start()
+            elif 'which day it is' in text:
+                tellDay()
+                start()
+            #elif 'image' in text or 'images' in text:
+                #webbrowser.open('https://www.google.com/search?tbm=isch&q=' + str(text))
+                #start()
+            elif 'video' in text or 'videos' in text:
+                webbrowser.open('https://www.google.com/search?tbm=vid&q=' + str(text))
+                start()
+            elif 'news' in text:
+                webbrowser.open('https://www.google.com/search?tbm=nws&q=' + str(text))
+                start()
+            elif 'download image' in text:
+                image_download()
+                start()
+            else: 
                 print(text)
                 speak('searching'+text)
-                pywhatkit.search(text)
+                if text == 'download image':
+                    image_download()
+                else:
+                    pywhatkit.search(text)
                 start()
 
         except Exception as e:
@@ -101,6 +230,3 @@ def start():
             start()
 speak('My name is KAKATONA')
 start()
-
-    
-'''https://machinelearningknowledge.ai/create-ai-voice-assistant-with-speech-recognition-python-project-source-code/'''
